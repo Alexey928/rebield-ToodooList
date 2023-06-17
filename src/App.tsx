8822f3077ab51bb7a04d277b8ad06587356544d5
Alexey928 <aleksey__tihakov@ukr.net>
2023-06-17 10:56:31 +0300

    start create base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import TooDooList from "./Components/TooDooList";
+import {v1} from "uuid";
+
 const styles = {
   too_doo_contayner:{
     display:"flex",
@@ -8,39 +10,60 @@ const styles = {
   }
 }
 
-const v1 = ():string =>{
-    return Math.random().toString()
-}
-
-export type filterValueType = "All"|"Active"|"Complited"
+export type filterValueType = "All" | "Active" | "Complited";
 
 export type toodooListType  = {
-
+     id:string,
+     title:string,
+     filter:filterValueType
 }
+
 type tasckType = {
     id:string,
     title:string,
     isDone:boolean,
 }
+
 type stateTascksType = {
     [key:string]:Array<tasckType>
 }
 
 
 function App() {
-    const [toodoLists, setToodoLists] = useState([{id:v1(), title:"list1", filter:"All"}]);
+    const Id1 = v1();
+    const Id2 = v1();
+    const Id3 = v1();
+    const [toodoLists, setToodoLists] = useState(
+        [  {id:Id1, title:"list1", filter:"All"},
+                    {id:Id2, title:"list2", filter:"Complited"},
+                    {id:Id3, title:"list3", filter:"Active"},
+        ]);
 
 
-    const [tascks, setTascks] = useState<stateTascksType>({
-        ["0.1123214142"]:[{id:v1(),title:"first tasck",isDone:false}]
+    const [tasks, setTascks] = useState<stateTascksType>({
+            [Id1]:[{id:v1(),title:"first tasck",isDone:false}],
+            [Id2]:[{id:v1(),title:"first tasck",isDone:true}],
+            [Id3]:[{id:v1(),title:"first tasck",isDone:false}],
     });
 
-    
-    
+
+    const fileterForRenderedTaskc = (toodoList:toodooListType) => {
+        switch (toodoList.filter) {
+            case "Active":
+                return tasks[toodoList.id].filter(t => !t.isDone);
+            case "Complited":
+                return tasks[toodoList.id].filter(t => t.isDone);
+            default:
+                return tasks[toodoList.id];
+       }}
+
     return (
-      <div style={styles.too_doo_contayner}>
-        <TooDooList />
-      </div>
+        <div>
+            <div style={styles.too_doo_contayner}>
+                to
+            </div>
+        </div>
+
 
   );
 }
